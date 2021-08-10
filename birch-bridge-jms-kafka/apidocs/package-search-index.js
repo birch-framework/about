305c74df8fed2452391445c30d00d426c1c9f3a7
Keivan Khalichi <keivan@khalichi.com>
2021-08-10 13:44:06 -0400

    Wagon: Adding site to repository

---
+++ birch-bridge-jms-kafka/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.birchframework.bridge"},{"l":"org.birchframework.bridge.dataformat"},{"l":"org.birchframework.bridge.jmstemplate"}]
